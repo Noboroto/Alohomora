8462a368974ee0b33e20f05215bb95335e61413f
David0403 <48942146+Noboroto@users.noreply.github.com>
2022-11-13 09:32:27 +0700

    Update

---
+++ source/commands/sendToChannel.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("send-to-this-channel")
+		.setDescription("Send a messsage to this channel!")
+		.addStringOption(Option =>
+			Option.setName("content")
+				.setDescription("message content")
+				.setRequired(true),
+		)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+		.setDMPermission(false),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		const targetChannel = interaction.channel;
+		const response = interaction.options.getString("content");
+		const message = {
+			content: response,
+		};
+		targetChannel.send(message);
+	},
+};
---
+++ source/commands/sendToSpecificChannel.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("send-to-specific-channel")
+		.setDescription("Send a messsage to specific channel!")
+		.addChannelOption(Option =>
+			Option.setName("destination")
+				.addChannelTypes(ChannelType.GuildText)
+				.setDescription("destination channel")
+				.setRequired(true),
+		)
+		.addStringOption(Option =>
+			Option.setName("content")
+				.setDescription("message content")
+				.setRequired(true),
+		)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+		.setDMPermission(false),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		const targetChannel = interaction.options.getChannel("destination");
+		const response = interaction.options.getString("content");
+
+		const message = {
+			content: response,
+		};
+		targetChannel.send(message);
+		await interaction.reply({
+			content: "Done!",
+			ephemeral: true,
+		});
+	},
+};
