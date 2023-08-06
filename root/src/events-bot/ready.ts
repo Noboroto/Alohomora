54b57f5dcf174ebf0ac49caa3e99a9372bfd9d33
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-08-06 12:39:18 +0700

    Update ready.ts

--- root/src/events-bot/ready.ts
+++ root/src/events-bot/ready.ts
@@ -8,8 +8,6 @@ export default {
 		const client = args[0] as Client;
 		console.log(`Ready! Logged in as ${client.user?.tag}`);
 
-		return;
-
 		await client.application.commands
 			.set([])
 			.then(() =>
